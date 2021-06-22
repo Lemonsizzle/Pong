f8549d1e8b0cc419a7a270b14dca1f19cc1ad83b
Zane Little-James <zane1092@gmail.com>
2021-06-22 18:15:08 +1200

    Add project files.

---
+++ Pong/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿
+namespace Pong
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.paddlePlayer = new System.Windows.Forms.PictureBox();
+            this.ball = new System.Windows.Forms.PictureBox();
+            this.paddleCPU = new System.Windows.Forms.PictureBox();
+            this.scoreP = new System.Windows.Forms.Label();
+            this.scoreC = new System.Windows.Forms.Label();
+            this.gameTime = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.paddlePlayer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.paddleCPU)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // paddlePlayer
+            // 
+            this.paddlePlayer.BackColor = System.Drawing.Color.Lime;
+            this.paddlePlayer.Location = new System.Drawing.Point(12, 186);
+            this.paddlePlayer.Name = "paddlePlayer";
+            this.paddlePlayer.Size = new System.Drawing.Size(25, 125);
+            this.paddlePlayer.TabIndex = 0;
+            this.paddlePlayer.TabStop = false;
+            // 
+            // ball
+            // 
+            this.ball.BackColor = System.Drawing.Color.White;
+            this.ball.Location = new System.Drawing.Point(350, 250);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(20, 20);
+            this.ball.TabIndex = 1;
+            this.ball.TabStop = false;
+            // 
+            // paddleCPU
+            // 
+            this.paddleCPU.BackColor = System.Drawing.Color.Red;
+            this.paddleCPU.Location = new System.Drawing.Point(745, 230);
+            this.paddleCPU.Name = "paddleCPU";
+            this.paddleCPU.Size = new System.Drawing.Size(25, 125);
+            this.paddleCPU.TabIndex = 2;
+            this.paddleCPU.TabStop = false;
+            // 
+            // scoreP
+            // 
+            this.scoreP.AutoSize = true;
+            this.scoreP.BackColor = System.Drawing.Color.Transparent;
+            this.scoreP.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.scoreP.ForeColor = System.Drawing.Color.Lime;
+            this.scoreP.Location = new System.Drawing.Point(142, 26);
+            this.scoreP.Name = "scoreP";
+            this.scoreP.Size = new System.Drawing.Size(36, 26);
+            this.scoreP.TabIndex = 3;
+            this.scoreP.Text = "00";
+            // 
+            // scoreC
+            // 
+            this.scoreC.AutoSize = true;
+            this.scoreC.BackColor = System.Drawing.Color.Transparent;
+            this.scoreC.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.scoreC.ForeColor = System.Drawing.Color.Red;
+            this.scoreC.Location = new System.Drawing.Point(606, 26);
+            this.scoreC.Name = "scoreC";
+            this.scoreC.Size = new System.Drawing.Size(36, 26);
+            this.scoreC.TabIndex = 4;
+            this.scoreC.Text = "00";
+            // 
+            // gameTime
+            // 
+            this.gameTime.Enabled = true;
+            this.gameTime.Interval = 20;
+            this.gameTime.Tick += new System.EventHandler(this.timerTick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.scoreC);
+            this.Controls.Add(this.scoreP);
+            this.Controls.Add(this.paddleCPU);
+            this.Controls.Add(this.ball);
+            this.Controls.Add(this.paddlePlayer);
+            this.Name = "Form1";
+            this.Text = "Pong Game 1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.downPress);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.upPress);
+            ((System.ComponentModel.ISupportInitialize)(this.paddlePlayer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.paddleCPU)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox paddlePlayer;
+        private System.Windows.Forms.PictureBox ball;
+        private System.Windows.Forms.PictureBox paddleCPU;
+        private System.Windows.Forms.Label scoreP;
+        private System.Windows.Forms.Label scoreC;
+        private System.Windows.Forms.Timer gameTime;
+    }
+}
+
---
+++ Pong/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pong
+{
+    public partial class Form1 : Form
+    {
+        bool goup;
+        bool godown;
+        
+        int speedCPU = 5;
+        int speedPlayer = 5;
+        int walk = 8;
+        int runM = 5;
+        bool running;
+
+        int ballx = 5;
+        int bally = 5;
+
+        int pPoints = 0;
+        int cPoints = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void downPress(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                goup = true;
+            }
+            
+            if (e.KeyCode == Keys.Down)
+            {
+                godown = true;
+            }
+
+            if (e.Modifiers == Keys.Shift || e.KeyCode == Keys.Shift)
+            {
+                running = true;
+            }
+        }
+
+        private void upPress(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                goup = false;
+            }
+            
+            if (e.KeyCode == Keys.Down)
+            {
+                godown = false;
+            }
+
+            if (e.Modifiers == Keys.Shift || e.KeyCode == Keys.Shift)
+            {
+                running = false;
+            }
+        }
+
+        private void timerTick(object sender, EventArgs e)
+        {
+            scoreP.Text = "" + pPoints;
+            scoreC.Text = "" + cPoints;
+
+            ball.Top -= bally;
+            ball.Left -= ballx;
+
+            paddleCPU.Top += speedCPU;
+
+            if(pPoints < 5)
+            {
+                if(paddleCPU.Top < 0 || paddleCPU.Top > 455)
+                {
+                    speedCPU = -speedCPU;
+                }
+            }
+            else
+            {
+                paddleCPU.Top = ball.Top + 30;
+            }
+
+            if(ball.Left < 0)
+            {
+                ball.Left = 434;
+                ballx = -ballx;
+                ballx -= 2;
+                cPoints++;
+            }
+            
+            if(ball.Left + ball.Width > ClientSize.Width)
+            {
+                ball.Left = 434;
+                ballx = -ballx;
+                ballx += 2;
+                pPoints++;
+            }
+
+            if(ball.Top < 0 || ball.Top + ball.Height > ClientSize.Height)
+            {
+                bally = -bally;
+            }
+
+            if(ball.Bounds.IntersectsWith(paddlePlayer.Bounds) || ball.Bounds.IntersectsWith(paddleCPU.Bounds))
+            {
+                ballx = -ballx;
+            }
+
+
+            if (running)
+            {
+                if (goup == true && paddlePlayer.Top > 0)
+                {
+                    paddlePlayer.Top -= walk * runM;
+                }
+
+                if (godown == true && paddlePlayer.Top < 455)
+                {
+                    paddlePlayer.Top += walk * runM;
+                }
+            }
+            else
+            {
+                if (goup == true && paddlePlayer.Top > 0)
+                {
+                    paddlePlayer.Top -= walk;
+                }
+
+                if (godown == true && paddlePlayer.Top < 455)
+                {
+                    paddlePlayer.Top += walk;
+                }
+            }
+
+            if (pPoints == 10)
+            {
+                gameTime.Stop();
+                MessageBox.Show("You win this game");
+            }
+
+            if(cPoints == 10)
+            {
+                gameTime.Stop();
+                MessageBox.Show("CPU wins, you lose");
+            }
+
+        }
+    }
+}
